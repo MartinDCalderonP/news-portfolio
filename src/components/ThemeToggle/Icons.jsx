9fa1834896c4d2b775e3334b9a19a0688e8f93a4
Martín <martincalderon92@outlook.com>
2025-01-16 14:20:33 -0300

    add Icons component to ThemeToggle for improved icon rendering

---
+++ src/components/ThemeToggle/Icons.jsx
@@ -0,0 +1,12 @@
+import { FiSun, FiMoon } from 'react-icons/fi'
+
+const Icons = () => {
+  return (
+    <> 
+      <FiSun className='sun' />
+      <FiMoon className='moon' />
+    </>
+  )
+}
+
+export default Icons
