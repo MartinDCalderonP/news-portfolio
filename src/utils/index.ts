3efd2039819e901fb26558f769966df998675de3
Martín <martincalderon92@outlook.com>
2025-01-16 23:51:33 -0300

    feat: implement date formatting in NewsCard and optimize news sorting logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,17 @@
 import type { News } from '../types/index'
 
 export const sortNewsList = (newsList: News[]) => {
-  const months: { [key: string]: number } = {
-    enero: 0,
-    febrero: 1,
-    marzo: 2,
-    abril: 3,
-    mayo: 4,
-    junio: 5,
-    julio: 6,
-    agosto: 7,
-    septiembre: 8,
-    octubre: 9,
-    noviembre: 10,
-    diciembre: 11
-  }
-
-  const sortedNewsList = newsList.toSorted((a, b) => {
-    const [dayA, monthA, yearA] = a.date.split(' de ')
-    const [dayB, monthB, yearB] = b.date.split(' de ')
-
-    const dateA = new Date(+yearA, months[monthA], +dayA).getTime()
-    const dateB = new Date(+yearB, months[monthB], +dayB).getTime()
-
-    return dateB - dateA
-  })
+  const sortedNewsList = newsList.toSorted(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
 
   return sortedNewsList
+}
+
+export const formatNewsDate = (date: string) => {
+  return new Date(date).toLocaleDateString('es-AR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
 }
