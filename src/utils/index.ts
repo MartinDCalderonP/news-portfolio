dd00b7cc1ad4dd5da2e36b9f74ad374983e0ee72
Martín <martincalderon92@outlook.com>
2025-01-22 21:55:33 -0300

    feat: add photographerUrl utility function to map photographer names to Instagram URLs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,3 +19,20 @@ export const formatNewsDate = (date: string) => {
 export const imageName = (url: string): string => {
   return url.split('/').pop() ?? ''
 }
+
+export const photographerUrl = (name: string): string => {
+  const instagram = 'https://www.instagram.com/'
+
+  const urls: Record<string, string> = {
+    'Daniela Hernández': instagram + 'danihc_ph',
+    'Pamela Pezo Malpica': instagram + 'pams.pm',
+    'Valentina Gomez': instagram + 'fotosmariaval',
+    'Clara Pérez Colman': instagram + 'p.cclara',
+    'Prensa Cele Fierro': instagram + 'cele_fierro',
+    'Camila Alonso Suárez / Archivo ANCCOM': instagram + 'camialonso.s',
+    'Gentileza Martín Zabala': instagram + 'zabamar',
+    'ARCHIVO TELAM': instagram + 'agencia_telam'
+  }
+
+  return urls[name] ?? ''
+}
